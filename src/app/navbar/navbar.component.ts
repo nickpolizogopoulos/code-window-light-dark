6a177cd7e74dbb543cb1fb375ff5437292a3eb88
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-12-08 18:00:01 +0200

    Add logic for hiding/show nav items and toggle for responsiveness

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,72 +1,87 @@
-import { Component, inject } from '@angular/core';
-
+import {
+  Component,
+  inject,
+  signal
+} from '@angular/core';
+  
 import { ModeService } from '../tools/mode.service';
-
-type NavLink = {
-  url: string;
-  name: string;
-  svgPath: string;
-};
+import {
+  navLinks,
+  type NavLink
+} from './nav-links';
 
 @Component({
   selector: 'header[Navbar]',
   standalone: true,
   template: `
   
-    @for (item of navLinks; track $index) {
-      <a 
-        style="color: {{ modeService.font() }};"
-        href="{{ item.url }}"
-        target="_blank"
-      >
-        <svg
-          [attr.viewBox]="item.name === 'More projects!' ? '0 0 24 24' : '0 0 16 16'"
-          width="17"
-          height="17"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
+    <section>
+      <div>
+        <input [hidden]="true" class="check-icon" id="check-icon" name="check-icon" type="checkbox">
+        <label (click)="onMenuToggle()" class="icon-menu" for="check-icon">
+          <div class="bar bar--1" style="background-color: {{ modeService.font() }};"></div>
+          <div class="bar bar--2" style="background-color: {{ modeService.font() }};"></div>
+          <div class="bar bar--3" style="background-color: {{ modeService.font() }};"></div>
+        </label>
+      </div>
+      <label for="check-icon" (click)="onMenuToggle()" [style.color]="modeService.mode() ? modeService.font() : modeService.font()">Menu</label>
+    </section>
+    @if (!linksHidden()) {
+      @for (item of navLinks; track $index) {
+        <a 
+          style="color: {{ modeService.font() }};"
+          href="{{ item.url }}"
+          target="_blank"
         >
-          <path [attr.d]="item.svgPath" />
-        </svg>
-        <span>{{ item.name }}</span>
-      </a>
+          <svg
+            [attr.viewBox]="item.name === 'More projects!' ? '0 0 24 24' : '0 0 16 16'"
+            width="17"
+            height="17"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+          >
+            <path [attr.d]="item.svgPath" />
+          </svg>
+          <span>{{ item.name }}</span>
+        </a>
+      }
     }
+
   
   `,
   host: {
     '[style.background-color]': 'modeService.background()',
+    '(window: resize)': 'onWindowResize()'
   },
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor() {
+    this.updateDrawerState();
+  }
+
   modeService = inject(ModeService);
 
-  private links: NavLink[] = [
-    {
-      url: 'https://nick-polizogopoulos.web.app/',
-      name: 'Portfolio',
-      svgPath: 'M3.29227 0.048984C3.47033 -0.032338 3.67946 -0.00228214 3.8274 0.125891L12.8587 7.95026C13.0134 8.08432 13.0708 8.29916 13.0035 8.49251C12.9362 8.68586 12.7578 8.81866 12.5533 8.82768L9.21887 8.97474L11.1504 13.2187C11.2648 13.47 11.1538 13.7664 10.9026 13.8808L8.75024 14.8613C8.499 14.9758 8.20255 14.8649 8.08802 14.6137L6.15339 10.3703L3.86279 12.7855C3.72196 12.934 3.50487 12.9817 3.31479 12.9059C3.1247 12.8301 3 12.6461 3 12.4414V0.503792C3 0.308048 3.11422 0.130306 3.29227 0.048984ZM4 1.59852V11.1877L5.93799 9.14425C6.05238 9.02363 6.21924 8.96776 6.38319 8.99516C6.54715 9.02256 6.68677 9.12965 6.75573 9.2809L8.79056 13.7441L10.0332 13.178L8.00195 8.71497C7.93313 8.56376 7.94391 8.38824 8.03072 8.24659C8.11753 8.10494 8.26903 8.01566 8.435 8.00834L11.2549 7.88397L4 1.59852Z'
-    },
-    {
-      url: 'https://www.linkedin.com/in/nickpolizogopoulos/',
-      name: 'LinkedIn',
-      svgPath: 'M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z'
-    },
-    {
-      url: 'https://github.com/nickpolizogopoulos/code-window-light-dark',
-      name: 'Source code',
-      svgPath: 'M7.49933 0.25C3.49635 0.25 0.25 3.49593 0.25 7.50024C0.25 10.703 2.32715 13.4206 5.2081 14.3797C5.57084 14.446 5.70302 14.2222 5.70302 14.0299C5.70302 13.8576 5.69679 13.4019 5.69323 12.797C3.67661 13.235 3.25112 11.825 3.25112 11.825C2.92132 10.9874 2.44599 10.7644 2.44599 10.7644C1.78773 10.3149 2.49584 10.3238 2.49584 10.3238C3.22353 10.375 3.60629 11.0711 3.60629 11.0711C4.25298 12.1788 5.30335 11.8588 5.71638 11.6732C5.78225 11.205 5.96962 10.8854 6.17658 10.7043C4.56675 10.5209 2.87415 9.89918 2.87415 7.12104C2.87415 6.32925 3.15677 5.68257 3.62053 5.17563C3.54576 4.99226 3.29697 4.25521 3.69174 3.25691C3.69174 3.25691 4.30015 3.06196 5.68522 3.99973C6.26337 3.83906 6.8838 3.75895 7.50022 3.75583C8.1162 3.75895 8.73619 3.83906 9.31523 3.99973C10.6994 3.06196 11.3069 3.25691 11.3069 3.25691C11.7026 4.25521 11.4538 4.99226 11.3795 5.17563C11.8441 5.68257 12.1245 6.32925 12.1245 7.12104C12.1245 9.9063 10.4292 10.5192 8.81452 10.6985C9.07444 10.9224 9.30633 11.3648 9.30633 12.0413C9.30633 13.0102 9.29742 13.7922 9.29742 14.0299C9.29742 14.2239 9.42828 14.4496 9.79591 14.3788C12.6746 13.4179 14.75 10.7025 14.75 7.50024C14.75 3.49593 11.5036 0.25 7.49933 0.25Z'
-    },
-    {
-      url: 'https://linktr.ee/nick_polizogopoulos',
-      name: 'More projects!',
-      svgPath: 'm13.511 5.853 4.005-4.117 2.325 2.381-4.201 4.005h5.909v3.305h-5.937l4.229 4.108-2.325 2.334-5.741-5.769-5.741 5.769-2.325-2.325 4.229-4.108H2V8.122h5.909L3.708 4.117l2.325-2.381 4.005 4.117V0h3.473v5.853zM10.038 16.16h3.473v7.842h-3.473V16.16z'
-    }
-  ];
+  linksHidden = signal<boolean>(false);
+  windowWidth = signal<number>(window.innerWidth);
+
+  onMenuToggle(): void {
+    this.linksHidden.set(!this.linksHidden());
+  }
 
   get navLinks(): NavLink[] {
-    return [ ...this.links ];
+    return [ ...navLinks ];
+  }
+
+  private updateDrawerState(): void {
+    const isMobile = this.windowWidth() < 970;
+    this.linksHidden.set(isMobile);
+  }
+  
+  private onWindowResize(): void {
+    this.windowWidth.set(window.innerWidth);
+    this.updateDrawerState();
   }
   
 }
