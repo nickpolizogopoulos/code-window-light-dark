7dc803bf5b8662e205a3070efff5394d9af06352
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-12-11 10:25:19 +0200

    Use the local storage to save the mode

--- src/app/tools/mode.service.ts
+++ src/app/tools/mode.service.ts
@@ -10,9 +10,30 @@ import { Palette } from "./palette";
     providedIn: 'root'
 })
 export class ModeService {
-    
+
+    private localStorageKey = 'code-window-mode';
+
     //* true = Light // false = Dark.
-    mode = signal<boolean>(false);
+    mode = signal<boolean>(this.loadSavedMode());
+
+    changeMode(): void {
+        const newMode = !this.mode();
+        this.mode.set(newMode);
+        this.saveMode(newMode);
+    }
+
+    private loadSavedMode(): boolean {
+        const savedMode = window.localStorage.getItem(this.localStorageKey);
+        return (
+              savedMode
+            ? JSON.parse(savedMode)
+            : false
+        );
+    }
+    
+    private saveMode(mode: boolean): void {
+        window.localStorage.setItem(this.localStorageKey, JSON.stringify(mode));
+    }
 
     private palette: Palette = {
         bgLight: 'rgba(255, 255, 255, 0.5)',
@@ -22,10 +43,6 @@ export class ModeService {
         commentedWhite: 'rgb(56, 56, 56)',
         commentedBlack: 'rgb(149, 149, 149)'
     };
-
-    changeMode(): void {
-        this.mode.set(!this.mode());
-    }
 
     background = computed<string>(() => (
           this.mode()
